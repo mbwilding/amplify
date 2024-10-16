b219267cc3afb0dd0c6a8c8b1e4985406a63bdaf
Matthew Wilding <mbwilding@gmail.com>
2024-10-16 12:38:00 +0800

    2024-10-16-12-38-00

--- pulumi/components/lambda.ts
+++ pulumi/components/lambda.ts
@@ -31,7 +31,7 @@ export function createLambda(
         authorizationType: "NONE",
         cors: {
             allowOrigins: ["*"],
-            allowMethods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"],
+            allowMethods: ["*"],
             allowHeaders: ["*"],
             exposeHeaders: ["*"],
             maxAge: 300,
